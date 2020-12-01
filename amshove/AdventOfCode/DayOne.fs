763030c3679e3ca6173cf0bba9c400b445d8ea08
Markus Amshove <scm@amshove.dev>
2020-12-01 21:47:52 +0100

    Loese Tag 1

---
+++ amshove/AdventOfCode/DayOne.fs
@@ -0,0 +1,25 @@
+﻿module DayOne
+
+
+let cartesian firstList secondList = 
+    firstList
+    |> List.collect (fun numberOfFirst -> 
+        secondList
+        |> List.map (fun numberOfSecond -> numberOfFirst, numberOfSecond)
+    )
+
+
+let combineWithEach list = cartesian list list
+
+let multiply (firstNumber, secondNumber) =
+    firstNumber * secondNumber
+
+let sumsTo2020 = function
+    | firstNumber, secondNumber -> firstNumber + secondNumber = 2020
+
+let solvePartOne input =
+    input
+    |> combineWithEach
+    |> List.filter sumsTo2020
+    |> List.map multiply
+    |> List.head
---
+++ amshove/AdventOfCode/Program.fs
@@ -0,0 +1,4 @@
+[<EntryPoint>]
+let main argv =
+    DayOne.solvePartOne Inputs.dayOne |> printfn "Solution of Day 1: %d"
+    0
