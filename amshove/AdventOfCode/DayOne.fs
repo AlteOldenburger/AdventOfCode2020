960598465bbeac483b31298f7ca1cf36f1bbab90
Markus Amshove <scm@amshove.dev>
2020-12-02 22:34:32 +0100

    Solve day 1 part 2

--- amshove/AdventOfCode/DayOne.fs
+++ amshove/AdventOfCode/DayOne.fs
@@ -5,12 +5,32 @@ let combineWithEach list = List.allPairs list list
 let multiply (firstNumber, secondNumber) =
     firstNumber * secondNumber
 
+
 let sumsTo2020 = function
-    | firstNumber, secondNumber -> firstNumber + secondNumber = 2020
+    | firstNumber, secondNumber, thirdNumber -> firstNumber + secondNumber + thirdNumber = 2020
+
+let twoSumTo2020 (first, second) = sumsTo2020 (first, second, 0)
 
 let solvePartOne input =
     input
     |> combineWithEach
-    |> List.filter sumsTo2020
+    |> List.filter twoSumTo2020
     |> List.map multiply
     |> List.head
+
+let combineThree list =
+    seq {
+        for i in list do
+            for j in list do
+                for k in list do
+                    yield i, j, k
+    }
+
+let multiplyThree (first, second, third) = first * second * third
+
+let solvePartTwo input =
+    input
+    |> combineThree
+    |> Seq.filter sumsTo2020
+    |> Seq.map multiplyThree
+    |> Seq.head
--- amshove/AdventOfCode/Program.fs
+++ amshove/AdventOfCode/Program.fs
@@ -1,4 +1,5 @@
 [<EntryPoint>]
 let main argv =
-    DayOne.solvePartOne Inputs.dayOne |> printfn "Solution of Day 1: %d"
+    DayOne.solvePartOne Inputs.dayOne |> printfn "Solution of Day 1 part 1: %d"
+    DayOne.solvePartTwo Inputs.dayOne |> printfn "Solution of Day 1 part 2: %d"
     0
