ea7982db66be5369da34f3530e7558bd46010f28
Mattes <mail@mattes-ossenbeck.de>
2020-12-10 23:28:04 +0100

    Loese Tag 06

--- ossenbeck/src/main/java/de/mattes/ossenbeck/day/Util.java
+++ ossenbeck/src/main/java/de/mattes/ossenbeck/day/Util.java
@@ -1,5 +1,11 @@
 package de.mattes.ossenbeck.day;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Util {
     public static final int NO_RESULT = -1;
 
@@ -21,5 +27,23 @@ public class Util {
             return isANumberAndBetween(height.substring(0, height.indexOf("i")), 59, 76);
         }
         return false;
+    }
+
+    public static Set<String> union(List<String> arr) {
+        return arr.stream()
+                  .flatMap(s -> Arrays.stream(s.split("")))
+                  .collect(Collectors.toSet());
+    }
+
+    public static Set<String> intersect(List<String> arr) {
+        var set = new HashSet<>(List.of(arr.get(0).split("")));
+
+        arr.stream()
+           .skip(1)
+           .map(s -> List.of(s.split("")))
+           .map(HashSet::new)
+           .forEach(set::retainAll);
+
+        return set;
     }
 }
